9836706d9cc6f99423ea7aba9d308541825870bd
7korobi <7korobi@gmail.com>
2020-08-16 04:50:15 +0900

    simple index provider

---
+++ lib/index-provider.tsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import React from 'react';
+import { useLocalStorage } from './storage';
+
+const IndexContext = createContext<[number, (idx: number)=> void]>(null as any)
+
+type IndexProviderProp = {
+  key: string
+  children: React.ReactNode
+}
+
+export function useIndex() {
+  return useContext(IndexContext)
+}
+
+export function IndexProvider({
+  key,
+  children,
+}: IndexProviderProp) {
+  const [index, setIndex] = useLocalStorage(key, 0)
+  useEffect(()=>{
+  }, [index])
+  return (
+    <IndexContext.Provider value={[index, setIndex]}>{children}</IndexContext.Provider>
+  )
+}
